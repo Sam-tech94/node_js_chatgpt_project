bada93781bc5f81c83a54a4dc530dda744537ace
Samvel <arman1993.samvel@gmail.com>
2023-05-13 12:28:48 +0400

    updated version

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const OpenAI = require('openai-api');
-const fs = require('fs');
-require('dotenv').config();
+import { Configuration,  OpenAIApi} from "openai";
+import fs from "fs";
 
-//Load OpenAI API key from enviroment vairables
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({
+  organization: "org-Gf8whMorZe2ki2KK6xgEVNKj",
+  apiKey: "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"
+});
 
-//Initialize OpenAI API client
-const openai = new OpenAI(OPENAI_API_KEY);
+const openai = new OpenAIApi(configuration);
 
 const sampleText = 'This is some sample text to be written to the input.txt file.';
 
@@ -16,21 +16,26 @@ fs.writeFile('input.txt', sampleText, (err) => {
 
   const inputText = fs.readFileSync('input.txt', 'utf-8');
 
-  const parameters = {
-    "prompt": inputText,
-    "temperature": 0.7,
-    "max_tokens": 60,
-    "top_p": 1,
-    "frequency_penalty": 0,
-    "presence_penalty": 0
-  };
+  console.log("input text", inputText)
 
-  openai.complete(parameters)
-    .then(response => {
-      const answer = response.data.choices[0].text.trim();
-      console.log(answer)
-    })
-    .catch(error => {
-      console.log(error)
-    });
+  openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: inputText}],
+  })
+  .then(response => {
+    const answer = response.data.choices[0]?.text?.trim();
+    if (answer) {
+      // console.log(answer);
+      fs.writeFile("output.txt", answer, (err) => {
+        if (err) throw err;
+        console.log("Output saved to output.txt");
+      });
+    } else {
+      console.log("Error: Empty response from OpenAI API");
+    }
+  })
+  .catch(error => {
+    console.log(error)
+  });
 });
+
